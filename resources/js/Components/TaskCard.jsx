496b423436261ad91ab4da8a37f6dd27b1822b69
Arseniy <avveeerage@gmail.com>
2023-12-25 19:26:11 +0300

    Added user validation for delete

--- resources/js/Components/TaskCard.jsx
+++ resources/js/Components/TaskCard.jsx
@@ -1,23 +1,29 @@
 import ApiService from '../services/ApiService';
+import { usePage } from '@inertiajs/react';
 
 export default function TaskCard({ task, user }) {
 
-const handleDelete = async () => {
-  try {
-    await ApiService.delete(`/tasks/${task.id}`);
-    window.location.reload();
-  } catch (error) {
-    console.error('Ошибка при удалении задачи:', error);
-  }
-};
+  const currentUser = usePage().props.auth.user;
+
+  const handleDelete = async () => {
+    try {
+      await ApiService.delete(`/tasks/${task.id}`);
+      window.location.reload();
+    } catch (error) {
+      console.error('Ошибка при удалении задачи:', error);
+    }
+  };
 
   return (
     <div className="relative flex flex-col items-start p-4 mt-3 bg-white rounded-lg cursor-pointer bg-opacity-90 group hover:bg-opacity-100" draggable="true">
-      <button onClick={handleDelete} className="absolute top-0 right-0 flex items-center justify-center hidden w-5 h-5 mt-3 mr-2 text-gray-500 rounded hover:bg-gray-200 hover:text-gray-700 group-hover:flex">
-        <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-        </svg>
-      </button>
+      {(currentUser.role === 'admin' || currentUser.id === user.id) && (
+        <button onClick={handleDelete} className="absolute top-0 right-0 flex items-center justify-center hidden w-5 h-5 mt-3 mr-2 text-gray-500 rounded hover:bg-gray-200 hover:text-gray-700 group-hover:flex">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+          </svg>
+        </button>
+      )}
       <span className="flex items-center h-6 px-3 text-xs font-semibold text-pink-500 bg-pink-100 rounded-full">{task.category}</span>
       <h4 className="mt-3 text-sm font-medium">{task.task}</h4>
       <div className="flex items-center w-full mt-3 text-xs font-medium text-gray-400">
